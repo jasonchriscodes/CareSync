fd9dd91014e4864bb7219322d1180bad146f043c
Jason Christian <jasonchristian1234@gmail.com>
2026-05-18 03:58:48 +1200

    feat: add table on admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,25 @@
+import { DataTable } from "@/components/DataTable";
 import { StatCard } from "@/components/StatCard";
+import { columns, Payment } from "@/components/table/columns";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    //...
+  ];
+}
 
 const Admin = async () => {
+  const data = await getData();
   const appointments = await getRecentAppointmentList();
 
   return (
@@ -51,6 +66,9 @@ const Admin = async () => {
             icon={"/assets/icons/cancelled.svg"}
           />
         </section>
+
+        {/* <DataTable columns={columns} data={appointments.documents} /> */}
+        <DataTable columns={columns} data={data} />
       </main>
     </div>
   );
